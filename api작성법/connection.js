bb4a942c607ba4aed5c52490e519ba4e59de3f08
BJS-kr <hahajeng1234@gmail.com>
2022-07-20 03:39:06 +0900

    update

--- api작성법/connection.js
+++ api작성법/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = async function () {
-  await mongoose.connect('mongodb://127.0.0.1:27017', { dbName:'test', user:'test', pass:'test' })
+  mongoose.connect('mongodb://127.0.0.1:27017', { dbName:'test', user:'test', pass:'test' })
     .then(
       x => console.log('> successfully opened the database')
     )
